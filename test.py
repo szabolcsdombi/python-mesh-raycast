82c604f022571f215ba013f21c44877b035a4ef6
Szabolcs Dombi <cprogrammer1994@gmail.com>
2019-03-23 16:27:19 +0200

    temporary test for iraycast

--- test.py
+++ test.py
@@ -8,4 +8,9 @@ triangles = np.array([
     [0.0, 1.0, 0.0],
 ], dtype='f4')
 
+index = np.array([
+    [0, 1, 2],
+], dtype='i4')
+
 print(mesh_raycast.raycast((0.0, 0.0, 2.0), mesh_raycast.normalize((0.1, 0.2, -1.0)), triangles))
+print(mesh_raycast.iraycast((0.0, 0.0, 2.0), mesh_raycast.normalize((0.1, 0.2, -1.0)), triangles, index))
